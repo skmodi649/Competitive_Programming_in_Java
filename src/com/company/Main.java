3316c224fa8842953ccfe99d8701688868cdd048
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2022-01-20 15:34:35 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the world of competitive programmig in Java");
+    }
+}
