dcae1b61b7c767ea7d15bb4ea34e61ed508e0120
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2022-01-24 21:16:47 +0530

    B Ordinary numbers program commit

---
+++ src/com/company/B_Ordinary_numbers.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class B_Ordinary_numbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        if (t >= 1 && t <= 10000) {
+            for (int a = 1; a <= t; a++) {
+                int num = sc.nextInt();
+                if(num >= 1 && num <= 9){
+                    System.out.println(num);}
+                else{
+                    int L = 1;
+                    int tmp = 0, ans = 0 , R = num;
+
+                    // Length of R
+                    int n = (int)Math.log10(R) + 1;
+
+                    for(int i = 0; i < n; i++)
+                    {
+
+                        // tmp has all digits as 1
+                        tmp = tmp * 10 + 1;
+
+                        // For each multiple of tmp
+                        // in range 1 to 9, check if
+                        // it present in range [L, R]
+                        for(int j = 1; j <= 9; j++)
+                        {
+                            if (L <= (tmp * j) && (tmp * j) <= R)
+                            {
+                                // Increment the required count
+                                ans++;
+                            }
+                        }
+                    }
+                    System.out.println(ans);
+                }
+            }
+        }
+    }
+}
