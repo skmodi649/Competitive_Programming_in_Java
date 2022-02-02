dd0e6862f14efab454b735cdf61c69a15c41b5bf
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2022-02-03 00:29:36 +0530

    Badminton serves program commit

---
+++ src/Codechef_Starters_24/Badminton_Serves.java
@@ -0,0 +1,22 @@
+package Codechef_Starters_24;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Badminton_Serves {
+    //  Badminton Serves
+
+
+
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            for(int a = 1 ; a <= t; a++){
+                int P = sc.nextInt();
+                int count = (P / 2) + 1;
+                System.out.println(count);
+            }
+        }
+    }
+
