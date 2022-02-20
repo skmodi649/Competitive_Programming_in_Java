02ea46cd1ec7c9b2e20e81ccbdd6982b36fff39f
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2022-02-21 02:01:08 +0530

    Codeforces Round 772 program commit

---
+++ src/Codeforces_Round_772/A.java
@@ -0,0 +1,17 @@
+package Codeforces_Round_772;
+
+import java.util.Scanner;
+
+public class A {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for(int i = 0 ; i < n ; i++)
+                arr[i] = sc.nextInt();
+            
+        }
+    }
+}
